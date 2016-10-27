a5eca9d2b530c5dd36c44eb530afc9a1ea2b9ad6
Matt McCullough <mmccullough@blackjet.ca>
2016-10-27 11:11:11 -0400

    Strip down full width row markup

--- lib/components/full-width-row.php
+++ lib/components/full-width-row.php
@@ -3,27 +3,11 @@
 	<tbody>
 		<tr>
 			<td>
-				<table border="0" cellpadding="0" cellspacing="0" width="100%">
+				<table class="full-width-row-inner" border="0" cellpadding="0" cellspacing="0"  style="width: 100%; max-width: 100%;" width=" 100%">
 					<tbody>
 						<tr>
-							<td id="templateHeader" valign="top">
-								<table border="0" cellpadding="0" cellspacing="0" style="min-width:100%;" width="100%">
-									<tbody>
-										<tr>
-											<td valign="top" align="center">
-												<table class="full-width-row-inner" border="0" cellpadding="0" cellspacing="0"  style="width: 100%; max-width: 100%;" width=" 100%">
-													<tbody>
-														<tr>
-															<td >
-																<?php echo $content; ?>
-															</td>
-														</tr>
-													</tbody>
-												</table>
-											</td>
-										</tr>
-									</tbody>
-								</table>
+							<td align="center">
+								<?php echo $content; ?>
 							</td>
 						</tr>
 					</tbody>
