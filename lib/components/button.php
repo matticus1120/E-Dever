d409614b3fa0c2c146de2a178db3b77e2701c4ef
Matt McCullough <mmccullough@blackjet.ca>
2016-10-27 10:54:43 -0400

    Strip down button and image:

--- lib/components/button.php
+++ lib/components/button.php
@@ -1,17 +1,9 @@
 <?php extract($variables) ?>
-<table border="0" cellpadding="0" cellspacing="0" width="100%" >
-	<tbody>
-		<tr>
-			<td <?php echo $align ?> >
-				<table class="<?php  echo implode(" ", $class_wrapper) ?>" border="0" cellpadding="0" cellspacing="0" style="<?php echo $class_wrapper_inline; ?>">
-					<tr>
-						<td class="<?php  echo implode(" ", $class_outer) ?>" align="center" valign="middle" style="<?php echo $class_outer_inline; ?>">
-							<a href="<?php echo $url ?>" target="_blank" class="button"  style="<?php echo $class_inline; ?>"><?php echo $content; ?></a>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</tbody>
-</table>
 
+<table class="<?php  echo implode(" ", $class_wrapper) ?>" border="0" cellpadding="0" cellspacing="0" style="<?php echo $class_wrapper_inline; ?>">
+	<tr>
+		<td class="<?php  echo implode(" ", $class_outer) ?>"  valign="middle" style="<?php echo $class_outer_inline; ?>">
+			<a href="<?php echo $url ?>" target="_blank" class="button"  style="<?php echo $class_inline; ?>"><?php echo $content; ?></a>
+		</td>
+	</tr>
+</table>
